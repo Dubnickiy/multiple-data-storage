3dca628e2bced14c25347ec9568b28789e9d8528
xfides <dmitrii.barabanov@ys.bet>
2022-05-15 17:06:17 +0300

    feat: add switcher between Dom\memory

--- script.js
+++ script.js
@@ -2,24 +2,43 @@ let lastId = 1;
 
 const list = document.querySelector(".list");
 const form = document.querySelector(".form");
+const switcherForm = document.querySelector(".switcher-form");
 const items = [];
 
+function handleDataInDom(data) {
+  const listItem = buildListItem(data);
+  list.append(listItem);
+}
 
-form.addEventListener("submit", ()=>{
-    const data = getFormData();
-    data.id = ++lastId;
-    items.push(data);
-    renderItems();
-})
+function handleDataInMemory(data) {
+  items.push(data);
+  renderItems();
+}
+
+const VALUES = {
+  dom: 'DOM',
+  memory: 'FrontMemory',
+};
+
+const handlers = {
+  [VALUES.dom]: handleDataInDom,
+  [VALUES.memory]: handleDataInMemory,
+}
+
+form.addEventListener("submit", function () {
+  const data = getFormData();
+  data.id = ++lastId;
+  handlers[switcherForm.mode.value](data);
+});
 
 function getFormData() {
-    return Object.fromEntries( new FormData(form));
+  return Object.fromEntries(new FormData(form));
 }
 
 function buildListItem(itemData) {
-    const elem = document.createElement("li");
-    elem.classList.add("list-item");
-    elem.innerHTML = `
+  const elem = document.createElement("li");
+  elem.classList.add("list-item");
+  elem.innerHTML = `
         <div class="cell cell-id">${itemData.id}</div>
         <div class="cell cell-text">${itemData.text}</div>
         <div class="cell cell-flag">
@@ -32,9 +51,9 @@ function buildListItem(itemData) {
             <button>🗑️</button>
         </div>
     `;
-    return elem;
+  return elem;
 }
 
 function renderItems() {
-    list.replaceChildren(...items.map(buildListItem));
+  list.replaceChildren(...items.map(buildListItem));
 }
